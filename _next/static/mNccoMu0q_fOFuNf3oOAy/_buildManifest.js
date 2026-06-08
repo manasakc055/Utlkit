de6a63d483da19249bdbefc5dc38ac8cf7f6d841
manasakc055 <manasakp214@gmail.com>
2026-06-08 17:19:55 +0530

    Updates

---
+++ _next/static/mNccoMu0q_fOFuNf3oOAy/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Utlkit//_next/:path+",
+        "destination": "/Utlkit/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
