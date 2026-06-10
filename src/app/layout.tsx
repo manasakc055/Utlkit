d0f439e8d3fc48df5c936f57a7fa931bb15207e2
manasakc055 <manasakp214@gmail.com>
2026-06-10 19:48:06 +0530

    remove comment from sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,3 +51,23 @@ export default function RootLayout({
 //     sitemap: `${baseUrl}/sitemap.xml`,
 //   };
 // }
+
+
+
+// import { MetadataRoute } from "next";
+
+// export default function sitemap(): MetadataRoute.Sitemap {
+//     const baseUrl = "https://utlkit.unistacx.com";
+//   return [
+//     {
+//       url: baseUrl,
+//       lastModified: new Date(),
+//       priority: 1,
+//     },
+//     {
+//       url: `${baseUrl}/dashboard`,
+//       lastModified: new Date(),
+//       priority: 0.8,
+//     },
+//   ];
+// } 
