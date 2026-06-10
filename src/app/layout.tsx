0a760c9a90d2346ae0c3c1566a233d0ee125fee7
manasakc055 <manasakp214@gmail.com>
2026-06-10 19:17:49 +0530

    move and updated folder sitemap, robot , updated config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,3 +36,18 @@ export default function RootLayout({
     </html>
   );
 }
+
+// import { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//         const baseUrl = "https://utlkit.unistacx.com";
+
+//   return {
+//     rules: {
+//       userAgent: "*",
+//       allow: "/",
+//       disallow: ["/contact",],
+//     },
+//     sitemap: `${baseUrl}/sitemap.xml`,
+//   };
+// }
